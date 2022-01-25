58bd0804b3269a6645cbd72611a546dc61857b07
rbarbanchoglez <98094078+rbarbanchoglez@users.noreply.github.com>
2022-01-25 16:22:12 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,12 +22,12 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "gcs_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
-  version = "~> 2.2"
-  project_id  = "${var.project}"
-  names = ["first", "second"]
-  prefix = "${var.project}"
+#module "gcs_buckets" {
+#  source  = "terraform-google-modules/cloud-storage/google"
+#  version = "~> 2.2"
+#  project_id  = "${var.project}"
+#  names = ["first", "second"]
+#  prefix = "${var.project}"
   #set_admin_roles = true
   #admins = ["group:foo-admins@example.com"]
   #versioning = {
@@ -36,7 +36,7 @@ module "gcs_buckets" {
   #bucket_admins = {
   #  second = "user:spam@example.com,eggs@example.com"
   #}
-}
+#}
 
 #module "vpc" {
 #  source  = "../../modules/vpc"
